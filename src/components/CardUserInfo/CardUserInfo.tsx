5b068fc613e07f4d29cd97ccbf6689b2b5cbb01b
Tiago Borges <tiago.borges@mutant.com.br>
2025-09-23 19:09:09 -0300

    feat: add CardUserInfo component to display user details

---
+++ src/components/CardUserInfo/CardUserInfo.tsx
@@ -0,0 +1,42 @@
+import { CardUserInfoProps } from "./types"
+
+export const CardUserInfo = ({
+    user,
+}: CardUserInfoProps) => {
+    return (
+        <div>
+            <div>
+                {/* <Image src={user?.avatar_url} alt={user?.login} /> */}
+
+                <div>
+                    <h1>{user?.name || ""}</h1>
+                    <p>@{user?.login}</p>
+                    <p>{user?.bio}</p>
+                </div>
+            </div>
+
+            <div>
+                <span>Seguidores:{" "}</span>
+                <span>{user?.following}</span>
+            </div>
+
+            <div>
+                <span>Seguindo:{" "}</span>
+                <span>{user?.followers}</span>
+            </div>
+
+            <div>
+                <span>Repositórios:{" "}</span>
+                <span>{user?.public_repos}</span>
+            </div>
+            <div>
+                <span>Localização:{" "}</span>
+                <span>{user?.location}</span>
+            </div>
+            <div>
+                <span>Membro desde:{" "}</span>
+                <span>{new Date(user?.created_at).getFullYear() || ""}</span>
+            </div>
+        </div>
+    )
+}
