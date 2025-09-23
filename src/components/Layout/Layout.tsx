51ebf771b43be4c38db64c0185919583f34c44aa
Tiago Borges <tiago.borges@mutant.com.br>
2025-09-23 19:10:25 -0300

    feat: integrate user search functionality into Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,19 @@
+"use client"
+import { useUserSearch } from "@/hooks/useUserSearch";
 import { Form } from "../Form/Form"
+import { CardUserInfo } from "../CardUserInfo/CardUserInfo";
 
 export const Layout = () => {
+    const { handleSearchUser, user, repo } = useUserSearch();
+
+    function handleSearchData(data: string) {
+        handleSearchUser(data);
+    }
+
     return (
         <>
-            <Form />
+            <Form handleSearchData={handleSearchData} />
+            <CardUserInfo user={user} repo={repo} />
         </>
     )
 }
