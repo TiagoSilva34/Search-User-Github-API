9e965be6551be08d98ccefe55e9154af2700abe5
Tiago Borges <tiago.borges@mutant.com.br>
2025-09-24 09:55:34 -0300

    feat: add CSS styling to input component

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,8 +1,10 @@
 import { IInputProps } from "./types";
+import styles from "./styles.module.css"
 
 export const Input = ({ type, placeholder, value, onChange }: IInputProps) => {
   return (
     <input
+      className={styles.input}
       type={type}
       placeholder={placeholder}
       value={value}
