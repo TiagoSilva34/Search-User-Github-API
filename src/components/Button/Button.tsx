b5568e6c90c4ee15b2d36820daa6ea2dbf58e577
Tiago Borges <tiago.borges@mutant.com.br>
2025-09-23 22:49:43 -0300

    feat: add disabled prop and styling to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,20 @@
+import styles from "./styles.module.css"
 interface IButtonProps {
     children: React.ReactNode;
     type: 'button' | 'submit' | 'reset';
+    disabled?: boolean;
 }
 
 export const Button = ({
     children,
-    type
+    type,
+    disabled
 }: IButtonProps) => {
     return (
         <button
             type={type}
-            className={''}
+            className={styles.button}
+            disabled={disabled}
         >
             {children}
         </button>
