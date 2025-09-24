ddc179facace3b23026b639f1754833440da4ee9
Tiago Borges <tiago.borges@mutant.com.br>
2025-09-24 16:19:44 -0300

    feat: add IUserProps interface to define user properties

---
+++ src/types/types.d.ts
@@ -0,0 +1,12 @@
+interface IUserProps {
+    name: string 
+    login: string 
+    location: string 
+    followers: number
+    following: number
+    bio: string
+    avatar_url: string
+    created_at: string 
+    id: number
+    public_repos: number
+}
