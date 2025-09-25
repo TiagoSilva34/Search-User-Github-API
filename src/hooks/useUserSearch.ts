7ef1e19e16c3f4a31557f8b0dbc92441a7238b9f
Tiago Borges <tiago.borges@mutant.com.br>
2025-09-25 16:14:03 -0300

    feat: refactor useUserSearch hook to improve caching logic and user state management

--- src/hooks/useUserSearch.ts
+++ src/hooks/useUserSearch.ts
@@ -1,37 +1,46 @@
 import { userService } from "@/services/userService";
 import { localStorageHandler } from "@/utils/browser/localStorage";
 import { useState } from "react";
-
+import { StorageParams } from "@/utils/storageParams/StorageParams";
 export const useUserSearch = () => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(false)
+  const [user, setUser] = useState<IUserProps>();
+  const [loading, setLoading] = useState(false);
+
+  async function handleSearchUser(username: string) {
+    setLoading(true);
+
+    const getUserSearchCache = localStorageHandler.retrieveData(
+      StorageParams.GET_USER_SEARCH_CACHE
+    )
+
+    const info = getUserSearchCache ? getUserSearchCache.info : [];
+
+    try {
+      const user = await userService.getUsers(username);
+
+      const isUserInCache = info.some((item: IUserProps) => item.id === user.id);
 
 
-    async function handleSearchUser(username: string) {
-        setLoading(true)
-        const getSearcUserCache = localStorageHandler.retrieveDataFromStorage('@getSearcUserCache')
-        try {
-            if (getSearcUserCache && username === getSearcUserCache) return getSearcUserCache
+      if (!isUserInCache) {
+        localStorageHandler.saveData(StorageParams.GET_USER_SEARCH_CACHE, [
+          user,
+          ...info,
+        ]);
+      }
 
-            const user = await userService.getUsers(username)
-
-            if (getSearcUserCache !== user) {
-                localStorageHandler.saveDataInStorage('@getSearcUserCache', [user])
-            }
-
-            setUser(user)
-        } catch(error) {
-            console.error("Error fetching user data:", error)
-            setUser(null)
-        } finally {
-            setLoading(false)
-        }
+      setUser(user);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      setUser(undefined);
+    } finally {
+      setLoading(false);
     }
+  }
 
-    return {
-        user, 
-        loading,
-        setUser,
-        handleSearchUser
-    }
-}
+  return {
+    user,
+    loading,
+    setUser,
+    handleSearchUser,
+  };
+};
