7acdcad53d23e9ec704f9a9e24e960d0ced759d6
Tiago Borges <tiago.borges@mutant.com.br>
2025-09-25 16:15:03 -0300

    feat: enhance CardUserInfo component with improved styling and layout adjustments

--- src/components/CardUserInfo/CardUserInfo.tsx
+++ src/components/CardUserInfo/CardUserInfo.tsx
@@ -1,42 +1,40 @@
 import { CardUserInfoProps } from "./types"
 import Image from "next/image"
+import styles from "./styles.module.css"
 
 export const CardUserInfo = ({
     user,
 }: CardUserInfoProps) => {
     return (
         <div>
-            <div>
+            <div className={styles.avatar}>
                 <Image src={user?.avatar_url} alt={user?.login} width={180} height={180} priority />
-
-                <div>
-                    <h1>{user?.name || ""}</h1>
-                    <p>@{user?.login}</p>
-                    <p>{user?.bio}</p>
-                </div>
             </div>
-
-            <div>
+            <div className={styles.username}>
+                <span>{user.name}</span>
+                <span>@{user.login}</span>
+            </div>
+            <div className={styles.bio}>
+                <span>{user.bio}</span>
+            </div>
+            <div className={styles.follow}>
                 <span>Seguidores:{" "}</span>
-                <span>{user?.following}</span>
-            </div>
-
-            <div>
+                <span>{user.following}</span>
+                <span>-</span>
                 <span>Seguindo:{" "}</span>
                 <span>{user?.followers}</span>
             </div>
 
-            <div>
-                <span>Repositórios:{" "}</span>
+            <div className={styles.repositories}>
+                <span>Repositórios públicos:{" "}</span>
                 <span>{user?.public_repos}</span>
             </div>
-            <div>
-                <span>Localização:{" "}</span>
-                <span>{user?.location}</span>
-            </div>
-            <div>
+            <div className={styles.member}>
                 <span>Membro desde:{" "}</span>
-                <span>{new Date(user?.created_at).getFullYear() || ""}</span>
+                <span>{new Date(user?.created_at).getFullYear()}</span>
+            </div>
+            <div className={styles.location}>
+                <span>{user?.location}</span>
             </div>
         </div>
     )
