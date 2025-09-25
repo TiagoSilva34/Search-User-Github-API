c1fab7ba5ba000086f13f234f655e5f6eba9ee76
Tiago Borges <tiago.borges@mutant.com.br>
2025-09-25 14:02:30 -0300

    feat: add unit tests for userService to validate API data fetching and error handling

---
+++ tests/units/src/service/userService.unit.spec.tsx
@@ -0,0 +1,39 @@
+import { userService } from "@/services/userService";
+import { handleApiConfig } from "@/services/apiConfig";
+
+jest.mock('@/services/apiConfig', () => ({
+    handleApiConfig: jest.fn()
+}))
+
+describe('userService', () => {
+    let mockGetUsers: jest.Mock;
+    beforeEach(() => {
+        mockGetUsers = jest.fn();
+
+        (handleApiConfig as jest.Mock).mockReturnValue({get: mockGetUsers});
+    })
+
+    afterEach(( ) => {
+        jest.clearAllMocks()
+    })
+
+    it('should fetch data when API call is successfully', async () => {
+        mockGetUsers.mockResolvedValue({data: [{id: 1, name: 'John'}]});
+
+        const data = await userService.getUsers('john');
+
+        expect(handleApiConfig).toHaveBeenCalledTimes(1)
+        expect(mockGetUsers).toHaveBeenCalledWith('/users/john')
+        expect(data).toEqual([{id: 1, name: 'John'}])
+    })
+
+
+    it('should throw an error when API call fails', async () => {
+        const username = 'johnDoe';
+        const error = new Error('API error');
+        mockGetUsers.mockRejectedValue(error);
+
+        await expect(userService.getUsers(username)).rejects.toThrow(error);
+        expect(mockGetUsers).toHaveBeenCalledWith(`/users/${username}`);
+    });
+})
