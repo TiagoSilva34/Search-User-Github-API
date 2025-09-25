75461c254ec39079920e718a76084651e567a36d
Tiago Borges <tiago.borges@mutant.com.br>
2025-09-25 16:14:47 -0300

    feat: update Button component to handle onClick prop and improve disabled state styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,18 @@
 import styles from "./styles.module.css"
-interface IButtonProps {
-    children: React.ReactNode;
-    type: 'button' | 'submit' | 'reset';
-    disabled?: boolean;
-}
+import { IButtonProps } from "./types";
 
 export const Button = ({
     children,
     type,
-    disabled
+    disabled,
+    onClick,
 }: IButtonProps) => {
     return (
         <button
             type={type}
-            className={styles.button}
+            className={disabled ? styles.disabled_btn : styles.enable_btn}
             disabled={disabled}
+            onClick={onClick}
         >
             {children}
         </button>
